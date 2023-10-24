4a196770e5ec332490088af0fdb1cb3265e4fe45
PakoPod <voypac@hotmail.com>
2023-10-23 22:17:58 -0600

    Page mau footer add

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,11 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById('navbar-content').innerHTML = data
     })
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('footer.html')
+    .then(response => response.text())
+    .then(data =>{
+        document.getElementById('footer-content').innerHTML = data
+    })
+});
